ffb1debedb24dff9c2c559a8239afec02ac6da24
Peter Veentjer <alarmnummer@gmail.com>
2024-05-20 08:36:38 +0300

    Lot more work on grammar

--- rubbish.asm
+++ rubbish.asm
@@ -1,11 +1,22 @@
-
+@banana
 .text
+
 foo: .word 10
+
+
+
 banana: .word 20
 .bananana
-add R1, R2, R3
-adr R1, foo
+
+add r1, R2, R3
+
+adr SP, foo
+
 ldr R1, [R1]
+        @ banana
+
+
 add R1, R2, #20
 .banana
 fanothervar: .word 3
+
